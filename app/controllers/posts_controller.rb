b1dbaa6257d5447ac41950bac7736e46b07eb6c4
salty-walty <waltonsj3@gmail.com>
2018-12-18 15:41:23 -0500

    added pagination, user posts page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,14 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
 
+  def user_posts
+    @user = User.find_by(username: params[:name])
+  end
+
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.all.page(params[:page])
   end
 
   # GET /posts/1
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'posts#index'
+
+  get  get '/:name' => 'posts#user_posts', as: :user_posts
 end
